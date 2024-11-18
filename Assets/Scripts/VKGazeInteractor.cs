92ea1c2262037e52bff496bef589b063f0c261f1
junjjang2 <junjjang2@twentyoz.kr>
2024-11-18 14:28:03 +0900

    vr 시선 추가

---
+++ Assets/Scripts/VKGazeInteractor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class VKGazeInteractor : MonoBehaviour
+    {
+        [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private Transform endPoint;
+        
+        [SerializeField] public LayerMask layers = ~0;
+
+        [SerializeField] private float maxDistance = 10f;
+
+        private WordText selectedWord;
+
+        public void Start()
+        {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.forward * 100);
+        }
+
+        public void Update()
+        {
+            lineRenderer.SetPosition(0, transform.position);
+
+            lineRenderer.SetPosition(1, transform.forward * maxDistance); 
+            endPoint.position = transform.position + transform.forward * maxDistance;
+            if (!Physics.Raycast(transform.position, transform.forward, out var hit, maxDistance, layers)) return;
+            
+            var interactable = hit.collider.GetComponent<WordText>();
+            if (interactable == null) return;
+                
+            lineRenderer.SetPosition(1, transform.position);
+            endPoint.position = interactable.transform.position;
+            
+            if(selectedWord != interactable)
+                selectedWord?.DeselectThis();
+            selectedWord = interactable;
+            interactable.SelectThis();
+        }
+    }
+}
